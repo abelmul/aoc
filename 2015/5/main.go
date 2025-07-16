6db84159defaeec497db7a3566c28cd3de9567d7
Abel Mulugeta <abelmulugeta2014@gmail.com>
2025-07-17 02:51:50 +0300

    2015: 5

---
+++ 2015/5/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/glenn-brown/golang-pkg-pcre/src/pkg/pcre"
+)
+
+/**
+Santa needs help figuring out which strings in his text file are naughty or nice.
+
+A nice string is one with all of the following properties:
+
+    It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
+    It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
+    It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
+
+For example:
+
+    ugknbfddgicrmopn is nice because it has at least three vowels (u...i...o...), a double letter (...dd...), and none of the disallowed substrings.
+    aaa is nice because it has at least three vowels and a double letter, even though the letters used by different rules overlap.
+    jchzalrnumimnmhp is naughty because it has no double letter.
+    haegwjzuvuyypxyu is naughty because it contains the string xy.
+    dvszwmarrgswjxmb is naughty because it contains only one vowel.
+
+How many strings are nice?
+
+--- Part Two ---
+
+Realizing the error of his ways, Santa has switched to a better model of determining whether a string is naughty or nice. None of the old rules apply, as they are all clearly ridiculous.
+
+Now, a nice string is one with all of the following properties:
+
+    It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+    It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
+
+For example:
+
+    qjhvhtzxzqqjkmpb is nice because is has a pair that appears twice (qj) and a letter that repeats with exactly one letter between them (zxz).
+    xxyxx is nice because it has a pair that appears twice and a letter that repeats with one between, even though the letters used by each rule overlap.
+    uurcxstgmygtbstg is naughty because it has a pair (tg) but no repeat with a single letter between them.
+    ieodomkazucvgmuy is naughty because it has a repeating letter with one between (odo), but no pair that appears twice.
+
+How many strings are nice under these new rules?
+*/
+
+func is_good_string_1(line string) int {
+	prev_char := '%'
+
+	found_vowels := 0
+	found_double_letters := 0
+
+	for _, char := range line {
+		switch char {
+		case 'a', 'e', 'i', 'o', 'u':
+			found_vowels++
+		}
+
+		if char == prev_char {
+			found_double_letters++
+		}
+
+		if (prev_char == 'a' && char == 'b') || (prev_char == 'c' && char == 'd') || (prev_char == 'p' && char == 'q') || (prev_char == 'x' && char == 'y') {
+			return 0
+		}
+
+		prev_char = char
+	}
+
+	if found_vowels > 2 && found_double_letters > 0 {
+		return 1
+	}
+
+	return 0
+}
+
+func is_good_string_2(line string) int {
+	regex_two_chars, _ := pcre.Compile(`(..).*\1`, 0)
+	regex_repeating_char, _ := pcre.Compile(`(.).\1`, 0)
+
+	if regex_two_chars.MatcherString(line, 0).Matches() && regex_repeating_char.MatcherString(line, 0).Matches() {
+		return 1
+	}
+	return 0
+}
+
+func main() {
+	good_strings_1 := 0
+	good_strings_2 := 0
+
+	for {
+		var line string
+
+		_, err := fmt.Scanln(&line)
+		if err == io.EOF {
+			break
+		}
+
+		good_strings_1 += is_good_string_1(line)
+		good_strings_2 += is_good_string_2(line)
+	}
+
+	fmt.Println(good_strings_1)
+	fmt.Println(good_strings_2)
+}
---
+++ 2015/go.mod
@@ -0,0 +1,5 @@
+module github.com/abelmul/aoc/2015
+
+go 1.24.4
+
+require github.com/glenn-brown/golang-pkg-pcre v0.0.0-20120522223659-48bb82a8b8ce
